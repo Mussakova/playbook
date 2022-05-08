09483b1d767379face6de96a7eb30656fd2a0547
Hirepan E <ginebrasyrel@gmail.com>
2022-05-08 08:54:04 -0500

    .map() con lowercase

---
+++ weekly_mission_2/examples/5_Map_lowercase.js
@@ -0,0 +1,6 @@
+// Ejemplo 5: Uso de Map para convertir todos los nombres de una lista a minúsculas
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const nameslower = names.map((i) => i.toLowerCase())
+
+console.log("Ejemplo 5: Uso de Map para convertir todos los nombres de una lista a minúsculas")
+console.log(nameslower)
