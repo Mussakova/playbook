713f369c893638427243f9f2e65891a6274b6d01
Hirepan E <ginebrasyrel@gmail.com>
2022-05-09 00:24:37 -0500

    Usando .sort() para ordenar a mis amigos

---
+++ weekly_mission_2/examples/15_Sort.js
@@ -0,0 +1,9 @@
+//Ejemplo 15: Uso de sort para ordenar elementos
+const products = ['Milk', 'Coffee', 'Sugar', 'Honey', 'Apple', 'Carrot']
+console.log("Ejemplo 15: Elementos ordernados usando SORT")
+console.log(products.sort())
+
+
+const amigos = ['Diana', 'Carlos', 'Daniel','José', 'Keyder', 'Alexia', 'Perla', 'Esmeralda', 'Jonathan']
+
+console.log(amigos.sort());
