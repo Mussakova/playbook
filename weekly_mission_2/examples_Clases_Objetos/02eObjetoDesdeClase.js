05216f75e307c0cdf2d1e176406bba00297d8cd3
Hirepan E <ginebrasyrel@gmail.com>
2022-05-10 15:24:05 -0500

    Creando objetos a partir de clases

---
+++ weekly_mission_2/examples_Clases_Objetos/02eObjetoDesdeClase.js
@@ -0,0 +1,11 @@
+// Ejemplo 2: Crear un objeto a partir de una clase
+class Pet {
+}
+const myPet1 = new Pet() // Puedo crear muchos objetos de la clase Pet
+console.log("Ejemplo 2: Crear un objeto a partir de una clase")
+console.log(myPet1) // un objeto de la clase Pet
+
+class amigos{
+}
+const amigos1 = new amigos()
+console.log(amigos1)
