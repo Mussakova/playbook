db273f90aec8d396ef5a7a05c68e3341ec12ca40
Hirepan E <ginebrasyrel@gmail.com>
2022-05-20 19:30:07 -0500

    Haciendo testing en JavaScript con JEST

---
+++ weekly_mission_3/my_launch_app/index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+const muss = new MissionCommander('Mussakova')
+const hirepan = new MissionCommander ('Hirepan')
+const diana = new MissionCommander('Diana')
+console.log(carlo.name);
+console.log(muss.name);
+console.log(hirepan.name);
+console.log(diana.name);
